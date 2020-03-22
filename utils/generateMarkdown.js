0c9b4b5ad9a3100815e5ff669bb90dc7abd9fa46
Arthur Freeman <Arthur@AJ-Freeman-MacBook.local>
2020-03-22 12:17:12 -0400

    Add initial markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,14 @@
 function generateMarkdown(data) {
   return `
-# ${data.title}
-
+# ${data.title},
+## ${data.description},
+## ${data.table},
+## ${data.install},
+## ${data.test},
+## ${data.usage},
+## ${data.contribution},
+## ${data.credit},
+## ${data.license},
 `;
 }
 
