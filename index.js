2a50e8bd17aae6a986c4330bae0d1336ba145806
Arthur Freeman <Arthur@AJ-Freeman-MacBook.local>
2020-03-23 09:40:18 -0400

    writeToFile start code

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 // username
 // project title
 // description of project
@@ -9,53 +10,74 @@ const inquirer = require("inquirer");
 // command run tests
 // Instructions
 // How to contribute
-inquirer
-  .prompt([
-      {type: "input", name: "github", message: "What is your GitHub username?"},
-      {type: "input", name: "title", message: "What is your Project title?"},
-      {type: "input", name: "description", message: "Please give a short description of the project."},
-      {type: "list", name: "license", message: "What License would you like to use?", choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]},
-      {type: "input", name: "install", message: "What command needs to be run to install dependencies?", default: "npm i"},
-      {type: "input", name: "test", message: "What command(s) should be run for tests?", default: "npm test"},
-      {type: "input", name: "usage", message: "What instructions does the user need to have to use the repo?"},
-      {type: "input", name: "contribute", message: "What does the user need to know about contributing to the repo?"}
-    ]).then((response) => {
-    console.log(response.github)
-    console.log(response.title)
-    console.log(response.description)
-    console.log(response.license)
-    console.log(response.install)
-    console.log(response.test)
-    console.log(response.usage)
-    console.log(response.contribute)
-    axios.get(`https://api.github.com/users/${response.github}`)
-      .then(data => {
-        console.log(data);
-        // data.data.login
-        // data.data.avatar_url
-        // data.data.email
-        // Add badges
-        // Write to file
-//         fs.writeFile("README.md", function(err) {
-//         })
-      })
+// inquirer
+//   .prompt([
+//       {type: "input", name: "github", message: "What is your GitHub username?"},
+//       {type: "input", name: "title", message: "What is your Project title?"},
+//       {type: "input", name: "description", message: "Please give a short description of the project."},
+//       {type: "list", name: "license", message: "What License would you like to use?", choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]},
+//       {type: "input", name: "install", message: "What command needs to be run to install dependencies?", default: "npm i"},
+//       {type: "input", name: "test", message: "What command(s) should be run for tests?", default: "npm test"},
+//       {type: "input", name: "usage", message: "What instructions does the user need to have to use the repo?"},
+//       {type: "input", name: "contribute", message: "What does the user need to know about contributing to the repo?"}
+//     ]).then((response) => {
+//     console.log(response.github)
+//     console.log(response.title)
+//     console.log(response.description)
+//     console.log(response.license)
+//     console.log(response.install)
+//     console.log(response.test)
+//     console.log(response.usage)
+//     console.log(response.contribute)
+//     axios.get(`https://api.github.com/users/${response.github}`)
+//       .then(data => {
+//         console.log(data);
+//         // data.data.login
+//         // data.data.avatar_url
+//         // data.data.email
+//         // Add badges
+//         // Write to file
+// //         fs.writeFile("README.md", function(err) {
+//   // if (err) {
+//     // return console.log(err)
+//   // }
+//   // console.log("Done!")
+// //         })
+//       })
+//   })
+const questions = [
+  {type: "input", name: "github", message: "What is your GitHub username?"},
+  {type: "input", name: "title", message: "What is your Project title?"},
+  {type: "input", name: "description", message: "Please give a short description of the project."},
+  {type: "list", name: "license", message: "What License would you like to use?", choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]},
+  {type: "input", name: "install", message: "What command needs to be run to install dependencies?", default: "npm i"},
+  {type: "input", name: "test", message: "What command(s) should be run for tests?", default: "npm test"},
+  {type: "input", name: "usage", message: "What instructions does the user need to have to use the repo?"},
+  {type: "input", name: "contribute", message: "What does the user need to know about contributing to the repo?"}
+];
+
+function writeToFile(fileName, data) {
+  fs.writeFile("README.md", data, function(err){
+    if (err) {
+      return console.log(err);
+    }
+    console.log("success")
   })
-// const questions = [
-//   {type: "input", name: "github", message: "What is your GitHub username?"},
-//   {type: "input", name: "title", message: "What is your Project title?"},
-//   {type: "input", name: "description", message: "Please give a short description of the project."},
-//   {type: "list", name: "license", message: "What License would you like to use?", choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]},
-//   {type: "input", name: "install", message: "What command needs to be run to install dependencies?", default: "npm i"},
-//   {type: "input", name: "test", message: "What command(s) should be run for tests?", default: "npm test"},
-//   {type: "input", name: "usage", message: "What instructions does the user need to have to use the repo?"},
-//   {type: "input", name: "contribute", message: "What does the user need to know about contributing to the repo?"}
-// ];
+}
 
-// function writeToFile(fileName, data) {
-// }
+function init() {
+  inquirer.prompt(questions
+    ).then((data) => {
+      console.log(data.github)
+      console.log(data.title)
+      console.log(data.description)
+      console.log(data.license)
+      console.log(data.install)
+      console.log(data.test)
+      console.log(data.usage)
+      console.log(data.contribute)
+      
+    })
+}
 
-// function init() {
-
-// }
-
-// init();
+init();
