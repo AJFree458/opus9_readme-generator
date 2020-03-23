aa5f990d4cfa24439846038aa2ca4bf11736c74b
Arthur Freeman <Arthur@AJ-Freeman-MacBook.local>
2020-03-22 22:43:07 -0400

    Fill out console.log

--- index.js
+++ index.js
@@ -11,13 +11,23 @@ const inquirer = require("inquirer");
 // How to contribute
 inquirer
   .prompt([
-    {
-      type: "input",
-      name: "github",
-      message: "What is your GitHub username?"
-    }
-  ]).then((response) => {
+      {type: "input", name: "github", message: "What is your GitHub username?"},
+      {type: "input", name: "title", message: "What is your Project title?"},
+      {type: "input", name: "description", message: "Please give a short description of the project."},
+      {type: "list", name: "license", message: "What License would you like to use?", choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]},
+      {type: "input", name: "install", message: "What command needs to be run to install dependencies?", default: "npm i"},
+      {type: "input", name: "test", message: "What command(s) should be run for tests?", default: "npm test"},
+      {type: "input", name: "usage", message: "What instructions does the user need to have to use the repo?"},
+      {type: "input", name: "contribute", message: "What does the user need to know about contributing to the repo?"}
+    ]).then((response) => {
     console.log(response.github)
+    console.log(response.title)
+    console.log(response.description)
+    console.log(response.license)
+    console.log(response.install)
+    console.log(response.test)
+    console.log(response.usage)
+    console.log(response.contribute)
     axios.get(`https://api.github.com/users/${response.github}`)
       .then(data => {
         console.log(data);
@@ -31,6 +41,7 @@ inquirer
       })
   })
 // const questions = [
+//   {type: "input", name: "github", message: "What is your GitHub username?"},
 //   {type: "input", name: "title", message: "What is your Project title?"},
 //   {type: "input", name: "description", message: "Please give a short description of the project."},
 //   {type: "list", name: "license", message: "What License would you like to use?", choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]},
