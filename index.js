8682617d6dd1a90c22d47ce276eb8ef03e0d3ae0
Arthur Freeman <Arthur@AJ-Freeman-MacBook.local>
2020-03-23 10:24:42 -0400

    Fill in init code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+const generateMarkDown = require("./utils/generateMarkdown");
 // username
 // project title
 // description of project
@@ -49,33 +49,43 @@ const questions = [
   {type: "input", name: "github", message: "What is your GitHub username?"},
   {type: "input", name: "title", message: "What is your Project title?"},
   {type: "input", name: "description", message: "Please give a short description of the project."},
-  {type: "list", name: "license", message: "What License would you like to use?", choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]},
+  {type: "list", name: "license", message: "What License would you like to use?", choices: ["MIT", "APACHE_2.0", "GPL_3.0", "BSD_3", "None"]},
   {type: "input", name: "install", message: "What command needs to be run to install dependencies?", default: "npm i"},
   {type: "input", name: "test", message: "What command(s) should be run for tests?", default: "npm test"},
   {type: "input", name: "usage", message: "What instructions does the user need to have to use the repo?"},
   {type: "input", name: "contribute", message: "What does the user need to know about contributing to the repo?"}
 ];
 
-function writeToFile(fileName, data) {
-  fs.writeFile("README.md", data, function(err){
-    if (err) {
-      return console.log(err);
-    }
-    console.log("success")
-  })
-}
+// function writeToFile(fileName, data) {
+//   fs.writeFile("README.md", data, function(err){
+//     if (err) {
+//       return console.log(err);
+//     }
+//     console.log("success");
+//   })
+// }
 
 function init() {
   inquirer.prompt(questions
-    ).then((data) => {
-      console.log(data.github)
-      console.log(data.title)
-      console.log(data.description)
-      console.log(data.license)
-      console.log(data.install)
-      console.log(data.test)
-      console.log(data.usage)
-      console.log(data.contribute)
+    ).then((answers) => {
+      console.log(answers.github)
+      console.log(answers.title)
+      console.log(answers.description)
+      console.log(answers.license)
+      console.log(answers.install)
+      console.log(answers.test)
+      console.log(answers.usage)
+      console.log(answers.contribute)
+      // Make a const for the GitHub Username
+      const userName = api.getUser(answers.github);
+      console.log(userName);
+      // Assign the answers to a data variable
+      const data = answers;
+      console.log(data);
+      //Set the markdown
+      // const markDown = generateMarkDown.generateMarkdown(data);
+      // Write it all to file
+      // writeToFile("README.md", markDown);
       
     })
 }
