60385411385c4053c69ec0825bfcbd13b353c440
Arthur Freeman <Arthur@AJ-Freeman-MacBook.local>
2020-03-23 13:05:08 -0400

    Change to code

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ function init() {
         const username = await api.getUser(answers.github);
         console.log(username);
         // Assign the answers to a data variable
-        const data = Object.assign({}, answers, username.data.login);
+        const data = answers;
         console.log(data);
         //Set the markdown
         const markDown = generate.generateMarkdown(data);
